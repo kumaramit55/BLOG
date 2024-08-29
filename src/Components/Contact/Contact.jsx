5b1daf52d1db8724610371e92ad8731a7953ec21
Amit Kumar <amitkverma551@gmail.com>
2024-08-29 19:04:23 +0530

    footer change

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import React, { useState } from 'react'
+
 
 export default function Contact() {
+  const[name,setName]=useState("")
+  const [email,setEmail]=useState("")
+  const [message,setMessage]=useState("")
+
+  const handelSubmit= async ()=>
+  {
+    console.log("SUBMIT FORM")
+    console.log(name,email,message)
+    try {
+      const url=`https://api.contentful.com/spaces/${import.meta.env.VITE_SPACE_ID}/environments/master/entries`
+      const headers={
+        Authorization: `Bearer ${import.meta.env.VITE_CMA}`,
+        "Content-Type": "application/vnd.contentful.management.v1+json",
+        "X-Contentful-Content-Type": "contact"
+      }
+  
+      const sendata={
+        fields:{
+          name:{
+            "en-US":name
+          },
+          email:{
+            "en-US":email
+          },
+          message:{
+            "en-US":message
+          }
+        }
+      }
+  
+      const response=await axios.post(url,sendata,{headers})
+      console.log(response)
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+   
+
+  }
+
   return (
+    
 
     <div className='flex flex-col gap-10'>
       {/* SOCIAL LINKS */}
@@ -19,26 +62,27 @@ export default function Contact() {
       </div>
 
       {/* CONTACT FORM */}
-   <form action="" className=''>
+   <form  className=''>
     <div className='flex flex-col w-4/5 sm:w-1/2 mx-auto border-b-2 border-slate-100 p-3 mb-4'>
       <label htmlFor="" className='text-left text-xl font-mono font-semibold'>Name</label>
-      <input type="text" className='text-xl rounded-md py-2 px-1 font-medium' placeholder='Jon Doe' />
+      <input type="text" className='text-xl rounded-md py-2 px-1 font-medium' placeholder='Jon Doe' 
+      onChange={(e)=>setName(e.target.value)}/>
     </div>
 
     <div className='flex flex-col w-4/5 sm:w-1/2 mx-auto  border-b-2 border-slate-100 p-3'>
       <label htmlFor="" className='text-left text-xl font-mono font-semibold'>Email Adrress</label>
-      <input type="text" className='text-xl rounded-md py-2 px-1 font-medium'placeholder='jondoes@gmail.com' />
+      <input type="text" className='text-xl rounded-md py-2 px-1 font-medium'placeholder='jondoes@gmail.com' onChange={(e)=>setEmail(e.target.value)} />
     </div>
+
     <div className='flex flex-col w-4/5  sm:w-1/2 mx-auto  border-b-2 border-slate-100 p-3'>
     <label htmlFor="" className='text-left text-xl font-mono font-semibold'>Message</label>
-    <textarea name="" id="" rows={4} cols={20} placeholder='Write Your Message Here' className='text-xl rounded-md p-1'></textarea>
+    <textarea name="" id="" rows={4} cols={20} placeholder='Write Your Message Here' className='text-xl rounded-md p-1' onChange={(e)=>setMessage(e.target.value)}></textarea>
    
     </div>
-
-    <div className='text-xl mt-4'><button className='bg-gray-400 p-3 rounded-sm font-serif'>Send message</button></div>
-    
-
-    
+    <div className='text-xl mt-4'>
+    <button className='bg-gray-400 p-3 rounded-sm font-serif' onClick={()=>handelSubmit()} >Send message
+    </button>
+    </div>
    </form>
    </div>
   )
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function Footer() {
+  const [email,setEmail]=useState("")
+  const notify= async ()=>{
+    console.log(email)
+    try {
+      const url=`https://api.contentful.com/spaces/${import.meta.env.VITE_SPACE_ID}/environments/master/entries`
+      const headers={
+        Authorization: `Bearer ${import.meta.env.VITE_CMA}`,
+        "Content-Type": "application/vnd.contentful.management.v1+json",
+        "X-Contentful-Content-Type": "notifications"
+      }
+  
+      const sendemail={
+        fields:{
+          
+          email:{
+            "en-US":email
+         
+        }
+      }
+    }
+  
+      const response=await axios.post(url,sendemail,{headers})
+      console.log(response)
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+
+  }
   return (
     <div className='mt-20  '>
       <h1 className='text-3xl font-bold'>Follow my journey</h1>
       <p className='text-gray-600 text-xl w-11/12  sm:w-1/2 mx-auto my-3  '>Get notified about updates and be the first to get early access to the new, safer, and smarter way to archive your files.</p>
       <div className='p-2 '>
-        <input type="text" placeholder='Enter Your Email' className='p-2 text-xl w-3/5 sm:w-1/2 ' />
-        <button className='bg-gray-400 text-white  p-2 text-xl'>Send It</button>
+        <input type="text" placeholder='Enter Your Email To Get Noitfy' className='p-2 text-xl w-3/5 sm:w-1/2 ' onChange={(e)=>setEmail(e.target.value)}/>
+        <button className='bg-gray-800 text-white  p-2 text-xl' onClick={()=>notify()}>Send It</button>
         
       </div>
-      <h5 className='text-gray-600 my-1  w-11/12 sm:w-1/2 mx-auto'>By subscribing to our newsletter you accept to receive recurring emails about our product and company</h5>
+      <h5 className='text-gray-500 my-1  w-11/12 sm:w-1/2 mx-auto'>By subscribing to our newsletter you accept to receive recurring emails about our product and company</h5>
 
  
     </div>
