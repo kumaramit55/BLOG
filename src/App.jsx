1b5e973a34cfd4dce4b5489765c81bcba0f79daf
Amit Kumar <amitkverma551@gmail.com>
2024-08-29 13:22:27 +0530

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LayOut from './Components/LayOut/LayOut'
+
+import About from './Components/About Me/About'
+import Contact from './Components/Contact/Contact'
+
+import Articles from './Components/Articles/Articles'
+import SingleArticle from './Components/SingleAticle/SingleArticle'
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LayOut />}>
+            <Route path='' element={<Articles/>} />
+            <Route path='AboutMe' element={<About />} />
+            <Route path='ContactMe' element={<Contact />} />
+            <Route path=':blogId' element={<SingleArticle/>}/>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+
+    </>
+  )
+}
+
+export default App
